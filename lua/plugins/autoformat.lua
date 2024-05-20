09b06d87b6a4ab6c73715dedf14db6802388cf40
Krztk <52879665+Krztk@users.noreply.github.com>
2024-05-20 21:43:02 +0200

    feat: increase formatters timeout

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -1,28 +1,29 @@
 local prettier = { "prettierd", "prettier" }
 
 return {
-    { -- Autoformat
-    'stevearc/conform.nvim',
-    opts = {
-      notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        javascript = { prettier },
-        typescript = { prettier },
-        javascriptreact = { prettier },
-        typescriptreact = { prettier },
-        css = { prettier },
-        html = { prettier },
-        json = { prettier },
-        jsonc = { prettier },
-        yaml = { prettier },
-        markdown = { prettier, "injected" },
-        graphql = { prettier },
-      },
-    },
-  },
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		opts = {
+			notify_on_error = false,
+			format_on_save = {
+				timeout_ms = 2000,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { prettier },
+				typescript = { prettier },
+				javascriptreact = { prettier },
+				typescriptreact = { prettier },
+				css = { prettier },
+				html = { prettier },
+				json = { prettier },
+				jsonc = { prettier },
+				yaml = { prettier },
+				markdown = { prettier, "injected" },
+				graphql = { prettier },
+			},
+		},
+	},
 }
+
