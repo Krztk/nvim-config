39b274719cd864705349ded701f0b86caf61f42f
Krztk <52879665+Krztk@users.noreply.github.com>
2024-08-09 12:55:20 +0200

    feat: mini files & comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,21 @@
 return {
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			options = {
+				enable_autocmd = false,
+			},
+		},
 		config = function()
 			require("mini.icons").setup()
+			require("mini.comment").setup({
+				options = {
+					custom_commentstring = function()
+						return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+					end,
+				},
+			})
 			local minifiles = require("mini.files")
 			minifiles.setup()
 
@@ -15,13 +28,40 @@ return {
 					minifiles.open()
 				end
 			end, { desc = "mini.files open current" })
+
+			local map_split = function(buf_id, lhs, direction)
+				local rhs = function()
+					-- Make new window and set it as target
+					local new_target_window
+					vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+						vim.cmd(direction .. " split")
+						new_target_window = vim.api.nvim_get_current_win()
+					end)
+
+					MiniFiles.set_target_window(new_target_window)
+				end
+
+				-- Adding `desc` will result into `show_help` entries
+				local desc = "Split " .. direction
+				vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					-- Tweak keys to your liking
+					map_split(buf_id, "gs", "belowright horizontal")
+					map_split(buf_id, "gv", "belowright vertical")
+				end,
+			})
 			-- Better Around/Inside textobjects
 			--
 			-- Examples:
 			--  - va)  - [V]isually select [A]round [)]paren
 			--  - yinq - [Y]ank [I]nside [N]ext [']quote
 			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
+			-- require("mini.ai").setup({ n_lines = 500 })
 
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			--
