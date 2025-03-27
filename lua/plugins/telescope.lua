ff77152cb52e2c2b169012ad0320e45a8456f608
Krztk <52879665+Krztk@users.noreply.github.com>
2025-03-27 12:22:48 +0100

    feat: telescope picker - recent git changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,63 @@
+local function find_recent_git_changes(opts)
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local conf = require("telescope.config").values
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+	local previewers = require("telescope.previewers")
+
+	local num_commits = opts and opts.num_commits or 3
+
+	local git_cmd = "git diff --name-only HEAD~" .. num_commits .. "..HEAD"
+
+	local find_command = function()
+		local handle = io.popen(git_cmd)
+		if not handle then
+			vim.notify("Failed to run git command", vim.log.levels.ERROR)
+			return {}
+		end
+
+		local result = handle:read("*a")
+		handle:close()
+
+		local files = {}
+		for file in string.gmatch(result, "[^\n]+") do
+			if file ~= "" then
+				table.insert(files, file)
+			end
+		end
+
+		return files
+	end
+
+	pickers
+		.new({}, {
+			prompt_title = "Files Changed in Last " .. num_commits .. " Commits",
+			finder = finders.new_table({
+				results = find_command(),
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry,
+						ordinal = entry,
+						path = entry,
+					}
+				end,
+			}),
+			sorter = conf.file_sorter({}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
+
+					vim.cmd("edit " .. selection.path)
+				end)
+				return true
+			end,
+		})
+		:find()
+end
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
@@ -52,6 +112,11 @@ return {
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			vim.keymap.set("n", "<leader>sc", function()
+				local num_commits = vim.v.count > 0 and vim.v.count or 3
+				find_recent_git_changes({ num_commits = num_commits })
+			end, { desc = "[S]earch [C]hanged Git Files (count commits, default 3)" })
 
 			vim.keymap.set("n", "<leader>/", function()
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
