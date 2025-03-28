c2f8069dbf44defa0805d50729302d67c15772a4
Krztk <52879665+Krztk@users.noreply.github.com>
2025-03-28 20:52:41 +0100

    chore: plugins update & formatting

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,7 +1,8 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking text',
-    group = vim.api.nvim_create_augroup('auto-command-highlight-yank', { clear = true }),
-    callback = function()
-      vim.highlight.on_yank()
-    end,
-  })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("auto-command-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
