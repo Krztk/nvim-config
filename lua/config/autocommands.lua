11db46f13675a5ac4c2d1109377248deb9d3c5f4
Krztk <52879665+Krztk@users.noreply.github.com>
2026-04-03 21:33:01 +0200

    feat: remove 'o' from format options

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,7 +1,22 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("auto-command-highlight-yank", { clear = true }),
+  group = augroup("auto-command-highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
+  end,
+})
+
+autocmd("BufEnter", {
+  group = augroup("FormatOptions", { clear = true }),
+  pattern = "*",
+  desc = "Set buffer local formatoptions.",
+  callback = function()
+    -- Don't continue comments on o/O in normal mode.
+    vim.opt_local.formatoptions:remove({
+      "o",
+    })
   end,
 })
